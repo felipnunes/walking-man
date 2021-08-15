d376b9690f5cef74b90a9d55f7d7e7401458f28e
Felipe <felipnunes155@gmail.com>
2021-08-14 21:22:41 -0300

    movimentaçao implementada(pequeno bug no sprite quando anda para a direita)

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include<SFML/Graphics.hpp>
 #include<iostream>
 
+void check_controls(sf::Sprite *sprite, float *actual_sprite, sf::IntRect *rect);
+const int movement = 4;
 int main() {
 
     sf::RenderWindow window(sf::VideoMode(500,500), "walking_man");
+    window.setFramerateLimit(60);
 
     sf::IntRect rect(0,0,96,160);
     sf::Texture texture;
     texture.loadFromFile("walking_man.png");
     sf::Sprite sprite(texture, rect);
+    float actual_sprite = 0;
+
 
     while(window.isOpen()) {
         sf::Event event;
@@ -23,12 +28,53 @@ int main() {
             window.close();
         }
 
+        check_controls(&sprite, &actual_sprite, &rect);
+        
         window.clear(sf::Color::White);
         window.draw(sprite);
         window.display();
     }
 
     return 0;
+}
+
+void check_controls(sf::Sprite *sprite,float *actual_sprite, sf::IntRect *rect) {
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+        sprite->move(0, -movement);
+        *actual_sprite += 0.2;
+        sprite->setTextureRect(sf::IntRect(0 + 96 *((int) *actual_sprite), 3 * 160,96,160));
+        if(*actual_sprite >= 12) {
+            *actual_sprite = 0;
+            sprite->setTextureRect(sf::IntRect(0, 3 * 160,96,160));
+        }
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+        sprite->move(-movement, 0);
+        *actual_sprite += 0.2;
+        sprite->setTextureRect(sf::IntRect(0 + 96 *((int) *actual_sprite),160,96,160));
+        if(*actual_sprite >= 12) {
+            *actual_sprite = 0;
+            sprite->setTextureRect(sf::IntRect(0,0,96,160));
+        }
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+        sprite->move(0, movement);
+        *actual_sprite += 0.2;
+        sprite->setTextureRect(sf::IntRect(0 + 96 *((int) *actual_sprite),0,96,160));
+        if(*actual_sprite >= 12) {
+            *actual_sprite = 0;
+            sprite->setTextureRect(sf::IntRect(0,0,96,160));
+        }
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        sprite->move(movement, 0);
+        *actual_sprite += 0.2;
+        sprite->setTextureRect(sf::IntRect(0 + 96 *((int) *actual_sprite),2 * 160,96,160));
+        if(*actual_sprite >= 12) {
+            *actual_sprite = 0;
+            sprite->setTextureRect(sf::IntRect(0,0,96,160));
+        }
+    }
 }
 
 
